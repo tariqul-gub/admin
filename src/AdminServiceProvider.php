23af66c9e6c53b8331562584bf1218c40910269f
Md. Tariqul Islam <tariqul9ict@gmail.com>
2023-05-19 12:35:28 +0600

    test

--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -15,8 +15,8 @@ class AdminServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../resources/views' => resource_path('views/vendor/admin'),
             __DIR__ . '/../public' => public_path(''),
-            __DIR__ . '/../config/admin.php' => config_path('admin.php'),
-        ], 'tariqul-gub-admin');
+        //     __DIR__ . '/../config/admin.php' => config_path('admin.php'),
+        // ], 'tariqul-gub-admin');
     }
     public function register(): void
     {

1091c404391e5a08bbb1a614b987db90e8648e50
Md. Tariqulk Islam <tariqul9ict@gmail.com>
2023-05-22 11:06:05 +0600

    file structure change

--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -8,17 +8,21 @@ class AdminServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'admin');
-        $this->mergeConfigFrom(__DIR__ . '/../config/admin.php', 'admin');
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'admin');
+        $this->mergeConfigFrom(__DIR__ . '/config/admin.php', 'admin');
 
         $this->publishes([
-            __DIR__ . '/../resources/views' => resource_path('views/vendor/admin'),
-            __DIR__ . '/../public' => public_path(''),
+            __DIR__ . '/resources/views' => resource_path('views/vendor/tariqul-gub/admin'),
+            __DIR__ . '/public' => public_path('vendor/tariqul-gub/admin'),
         ], 'tariqul-gub-admin-views');
 
         $this->publishes([
-            __DIR__ . '/../config/admin.php' => config_path('admin.php'),
+            __DIR__ . '/public' => public_path('vendor/tariqul-gub/admin'),
+        ], 'tariqul-gub-admin-assets');
+
+        $this->publishes([
+            __DIR__ . '/config/admin.php' => config_path('admin.php'),
         ], 'tariqul-gub-admin-config');
     }
     public function register(): void
--- src/config/admin.php
+++ src/config/admin.php
@@ -4,12 +4,12 @@ return [
     'css' => [
         'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,700,0,0',
         'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css',
-        config('app.url', "https:://127.0.0.1:8000") . '/vendor/tariqul-gub/admin/public/admin/scss/main.css',
+        asset('/vendor/tariqul-gub/admin/scss/main.css'),
     ],
     'js' => [
         'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js',
         'https://code.jquery.com/jquery-3.6.0.min.js',
-        config('app.url', "https:://127.0.0.1:8000") . '/vendor/tariqul-gub/admin/public/admin/js/admin.js',
+        asset('/vendor/tariqul-gub/admin/js/admin.js'),
     ],
     'logo' => asset('img/logo.svg'),
 
